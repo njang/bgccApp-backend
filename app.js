865f13121dc6cda907fb3b2db964621517e544c0
njang <neo.w.jang@gmail.com>
2018-02-05 17:31:13 -0600

    Restart from basic express-api app

--- app.js
+++ app.js
@@ -1,54 +1,16 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const app = express();
-const mongoose = require('mongoose');
-const logger = require('morgan');
-// const passport = require('passport');
-// const expressSession = require('express-session');
-// const User = require('./models/user');
-// const findOrCreate = require('mongoose-findorcreate');
-// const index = require('./routes/index');
-const users = require('./routes/users');
-const ENV      = require('./env');
+var users = require('./routes/users');
 
+var app = express();
 
-
-// Middleware
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(cookieParser());
-// app.use(passport.initialize());
-// app.use(passport.session());
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use('/', index);
+app.use(cookieParser())
+
 app.use('/api/v1/users', users);
-mongoose.connect(ENV.MONGODB_URI); 
-
-/**********
- * SERVER *
- **********/
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   let err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
 module.exports = app;
