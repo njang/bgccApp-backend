685fb9e100de7c80b7be8294d49ba92215bf7e5e
njang <neo.w.jang@gmail.com>
2018-02-05 15:21:04 -0600

    Revise app.js

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
-var users = require('./routes/users');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const app = express();
+const mongoose = require('mongoose');
+// const logger = require('morgan');
+// const passport = require('passport');
+// const expressSession = require('express-session');
+// const User = require('./models/user');
+// const ENV = require('./app-env');
+// const findOrCreate = require('mongoose-findorcreate');
 
-var app = express();
+// const index = require('./routes/index');
+const users = require('./routes/users');
 
+// Middleware
+app.use(cookieParser());
+// app.use(expressSession({ secret: 'mySecretKey' }));
+// app.use(passport.initialize());
+// app.use(passport.session());
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser())
 
+// app.use('/', index);
 app.use('/api/v1/users', users);
+
+
+/**********
+ * SERVER *
+ **********/
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
